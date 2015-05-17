e5588602bb6f1b66895f6e4e9ff6d7bb623b3cbb
Ubuntu <ubuntu@ip-172-31-17-118.us-west-2.compute.internal>
2015-05-17 13:36:38 +0000

    Functionality to perform Customer mapping Functionality to generate the final csv file from the SSOT.

--- ssot.php
+++ ssot.php
@@ -89,10 +89,12 @@ function generate_money_pivot_new($res) {
 function verify_revenue_adjustment_new($res){
     
     generate_money_pivot_new($res);
-
+    
+    $handle = @fopen("final.csv", "w+");
+  
     $NewRevenue=0;
     $OldRevenue=0;
-    
+    $i=0;
     foreach ($res as $list) {
 	$Category = $list[4];
 	$SO_Channel_Code = $list[2];
@@ -106,8 +108,15 @@ function verify_revenue_adjustment_new($res){
 	$Segment = $list[5];
 	$Product_Family=$list[9];
 	$OldRevenue += $Revrec_Net;
-	$NewRevenue += adjustRevenue($Revrec_Net, $Category, $Revrec_Type, $SO_Channel_Code, $Product_Family, $Segment, $P4_Reporting_Ship_to_GEO);
+	$adjRev =  adjustRevenue($Revrec_Net, $Category, $Revrec_Type, $SO_Channel_Code, $Product_Family, $Segment, $P4_Reporting_Ship_to_GEO);
+	$NewRevenue += $adjRev;
+	if ($i != 0 ){
+	    $list[14] = $adjRev;
+	}
+	$i = 1;
+	fputcsv($handle,$list);
     }
+    fclose($handle);
     print "Old Revenue = ". round($OldRevenue,0)."\n";
     print "New Revenue = ". round($NewRevenue,0)."\n";
     return ($OldRevenue == $NewRevenue);
@@ -148,8 +157,42 @@ function verify_revenue_adjustment($filename = "test1.csv"){
     return ($OldRevenue == $NewRevenue);
 }
 
+function eu_peug($filename = "EU_PEUG.csv"){
+    
+    global $eu_peug;
 
-function merge_ssot() {
+    $handle = @fopen($filename, "r");
+    
+    if (!$handle) {
+	print "Unable to open file\n";
+	exit;
+    }
+    while (($buffer = fgets($handle, 4096)) !== false) {
+	$list = str_getcsv($buffer, ",", '"');
+//	$pe = preg_replace("/[^A-Za-z0-9 ]/", '', $list[0]);
+//	$peug = preg_replace("/[^A-Za-z0-9 ]/", '', $list[1]);
+	$eu_peug[$list[0]] = $list[1];
+    }
+}
+
+function peug_vert($filename = "PEUG_VERT.csv"){
+
+    global $peug;
+
+    $handle = @fopen($filename, "r");
+    
+    if (!$handle) {
+	print "Unable to open file\n";
+	exit;
+    }
+    while (($buffer = fgets($handle, 4096)) !== false) {
+	$list = str_getcsv($buffer, ",", '"');
+	$peug[$list[0]][0]=$list[1];
+	$peug[$list[0]][1]=$list[2];
+    }
+}   
+
+function merge_ssot($quarter="Q115") {
 
     global $res;
     
@@ -170,7 +213,7 @@ function merge_ssot() {
 	}
 
 	if ($product == 1){
-	    $header= array("Parent Enduser Group","Rev_Rec_QTR","SO Channel Code","P4 Reporting Ship to GEO","Category","Segment","Vertical","Sub Vertical","Reportable Business","Product Family","Product Line Code","Product Line Desc","Revrec Net$","Revrec Cost$","VertRev_Final","Revrec Type");
+	    $header= array("Parent Enduser Group","Rev_Rec_QTR","SO Channel Code","P4 Reporting Ship to GEO","Category","Segment","Vertical","Sub Vertical","Reportable Business","Product Family","Product Line Code","Product Line Desc","Revrec Net\$","Revrec Cost\$","VertRev_Final","Revrec Type");
 	    
 	    if (($buffer = fgets($handle, 4096)) !== false) {
 		$list = str_getcsv($buffer, ",", '"');
@@ -183,22 +226,42 @@ function merge_ssot() {
 		    }
 		}
 	    }
-	    //print implode(',', $header)."\n";
+//	    print implode(',', $header)."\n";
 	    $i=0;
 	    $res[$i]=$header;
+	}else {
+	    // read the first line from "Service.csv" and ignore it. This is the header.
+	    $buffer = fgets($handle, 4096);
 	}
 	while (($buffer = fgets($handle, 4096)) !== false) {
 	    $i++;
 	    $list = str_getcsv($buffer, ",", '"');
 	    foreach ($header as $hdr_str) {
 		if ($index[$hdr_str] == -1){
-		    if ($hdr_str == "Category"){
-			if ($product == 1) {
-			    $res[$i][] = "Product";
+		    switch ($hdr_str) {
+			case "Category":
+			    if ($product == 1) {
+				$res[$i][] = "Product";
 			}else {
 			    $res[$i][] = "Service";
-			}
-		    }else {
+			    }
+			break;
+		        case "Parent Enduser Group":
+			    $res[$i][] = $eu_peug[$list[4]];
+			break;
+		        case "Vertical":
+			    $res[$i][] = $peug[$eu_peug[$list[4]]][0];
+			break;
+		        case "Sub Vertical":
+			    $res[$i][] = $peug[$eu_peug[$list[4]]][1];
+			break;
+		        case "Rev_Rec_QTR":
+			    $res[$i][] = $quarter;
+			break;
+		        case "Revrec Type":
+			    $res[$i][] = $list[0];
+			break;
+		      default:
 			$res[$i][] = "To be filled";
 		    }
 		}else {
@@ -208,6 +271,8 @@ function merge_ssot() {
 	}    
     }
 }
-merge_ssot("Service.csv", 0);
+eu_peug();
+peug_vert();
+merge_ssot();
 verify_revenue_adjustment_new($res);
 ?>
